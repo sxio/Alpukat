babd175414a7c1f617776aca177507657ea0c9ad
Kenedy Lukito <kenedylukito@yahoo.com>
2016-10-09 21:24:05 +0700

    prototype : penambahan dr. Dokter 4 ke list

--- list.php
+++ list.php
@@ -152,7 +152,52 @@
 							</div>
 						</div>
 						<!-- end of dokter 3 -->
+						<div class="row">
+							<div class="col-sm-3">
+								<img class="doctors" src="img/user.jpg">
+							</div>
+							<div class="col-sm-5 profil_singkat">
+								<h2 class="nama"><a href="#lamanprofildokternya">dr. Dokter 4</a></h2>
+								<h3 class="bidang">Spesialis : Ahli Bedah</h3>
+								<h4 class="pengalaman">Pengalaman : 100 Tahun</h4>
+								<!-- rating system -->
+								<div class="stars">
+								  	<form action="">
+								    	<input class="star star-5" id="star-54" type="radio" name="star"/>
+									    <label class="star star-5" for="star-54"></label>
+									    <input class="star star-4" id="star-44" type="radio" name="star"/>
+									    <label class="star star-4" for="star-44"></label>
+									    <input class="star star-3" id="star-34" type="radio" name="star"/>
+									    <label class="star star-3" for="star-34"></label>
+									    <input class="star star-2" id="star-24" type="radio" name="star"/>
+									    <label class="star star-2" for="star-24"></label>
+									    <input class="star star-1" id="star-14" type="radio" name="star"/>
+									    <label class="star star-1" for="star-14"></label>
+									 </form>
+								</div>
+								<!-- end of rating system -->
+								<div class="hargadanwaktu">
+									<div class="harga">
+										<i class="fa fa-money fa-fw"></i> <span class="nilai"><strong>Rp 100.000,-</strong></span>
+									</div>
+									<div class="waktu">
+										<i class="fa fa-clock-o fa-fw"></i>
+										<div class="jadwal nilai">Senin - Jumat</div>
+										<div class="waktu_kerja">08.00 - 22.00</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-4">
+								<div class="lokasi">
+									<h4>Tempat Praktek</h4>
+									<img class="gbr_lokasi" src="img/location.png">
+									<img class="gbr_lokasi" src="img/location.png">
+									<img class="gbr_lokasi" src="img/location.png">
+								</div>
+							</div>
+						</div>
 					</div>
+					<!-- end of dokter 4 -->
 				</div>
 				<!-- end of query -->
 				<div class="col-sm-3">
