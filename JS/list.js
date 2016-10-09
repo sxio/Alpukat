e8ed98801d385ddce411a1d52e8a1d7985b7387b
sxio <chronolegionaire1996@gmail.com>
2016-10-09 20:00:45 +0700

    perbaikan syntax list.js

--- JS/list.js
+++ JS/list.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
     $("#accordion").hide();
     });
-}); 
 $(document).ready(function(){
     $("#drop").click(function(){
         $(this).hide();
     });
-}); 
+});
