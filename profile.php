c1a4f20a1300bf03439fd5ede819a230e98a4194
sxio <chronolegionaire1996@gmail.com>
2016-10-09 17:25:51 +0700

    penambahan profile

---
+++ profile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Doctor Profile</title>
+	<?php require __DIR__."/PARTIAL/head.php"; ?>
+
+	<link rel="stylesheet" type="text/css" href="CSS/profile.css">
+	<script src="JS/main.js"></script>
+</head>
+<body>
+	<?php require __DIR__."/PARTIAL/nav.php" ?>
+
+	<section id="content">
+		<div class="container-fluid">
+
+		</div>
+	</section>
+</body>
+</html>
