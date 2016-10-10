f3d284e5655cbef177d12f6f891c8a70473858f0
Kenedy Lukito <kenedylukito@yahoo.com>
2016-10-10 14:28:59 +0700

    prototype : ganti font

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<?php require __DIR__."/PARTIAL/nav.php" ?>
 
 	<div class="jumbotron text-center">
-	  <h1>AVOCADO</h1>
+	  <h1>Avocado</h1>
 	  <h3>We specialize in Health Care Application</h3>
 	  <form class="form-inline" action="list.php">
 	    <input type="email" class="form-control" size="50" placeholder="Doctor`s Name or Hospital`s Name" required>
