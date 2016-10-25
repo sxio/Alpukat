4666888d6f0ff3c3460e2ace7e41f29bd4275d7a
sxio <chronolegionaire1996@gmail.com>
2016-10-25 23:16:45 +0700

    add plugin chatting

--- web/js/main.js
+++ web/js/main.js
@@ -14,7 +14,16 @@ $(document).ready(function(){
   }
   setInterval(blinker,1000);
   //stephen 25-10-16
-});
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/580f83ca2d3b340651816eb7/default';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+    })();
+  });
 function blinker(){
   $('.blink_me').fadeOut(500);
   $('.blink_me').fadeIn(500);
