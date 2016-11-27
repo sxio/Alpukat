e7286225de6b5a28a2e15ca5d91f43c8572edff6
sxio <stephen.huang96@gmail.com>
2016-11-27 16:27:49 +0700

    +link di nav (user)

--- application/views/home.php
+++ application/views/home.php
@@ -22,15 +22,7 @@
 			<h2 class="text-center"><i class="fa fa-newspaper-o fa-fw"></i> News</h2>
 				<div class="col-sm-6 leftedge">
 					<div id="myCarousel" class="carousel slide" data-ride="carousel">
-					  <!-- Indicators -->
-					  	<!-- <ol class="carousel-indicators">
-							<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-						    <li data-target="#myCarousel" data-slide-to="1"></li>
-						    <li data-target="#myCarousel" data-slide-to="2"></li>
-						    <li data-target="#myCarousel" data-slide-to="3"></li>
-					  	</ol> -->
-
-						  <!-- Wrapper for slides -->
+						<!-- Wrapper for slides -->
 					  	<div class="carousel-inner" role="listbox">
 						    <div class="item active">
 						      <img src="<?php echo base_url() ?>assets/img/stomach.jpg" alt="perut">
@@ -90,16 +82,6 @@
 						</div>
 						<iframe width="560" height="315" src="https://www.youtube.com/embed/9G95EYym4NM" frameborder="0" allowfullscreen></iframe>
 
-						  <!-- Left and right controls -->
-						  <!-- <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-						    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-						    <span class="sr-only">Previous</span>
-						  </a>
-
-						  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-						    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-						    <span class="sr-only">Next</span>
-						  </a> -->
 						</div>
 						<a href="#" class="news">Go to News Page &nbsp;&nbsp;<i class="fa fa-long-arrow-right"></i></a>
 					</div>
