88d1d231e78edc59aec55344bf8672a62b0296e1
Kenedy Lukito <kenedylukito@yahoo.com>
2016-11-29 19:59:41 +0700

    Homepage semi-final 5

--- application/views/home.php
+++ application/views/home.php
@@ -17,6 +17,23 @@
 	  </form>
 	</div>
 	<section id="startchange">
+		<h1 class="text-center">Here Are  Our Best Doctors of The Month</h1>
+		<div class="row text-center doc">
+			<div class="col-sm-4">
+				<a href=""><img src="<?php echo base_url() ?>assets/img/doctor.jpg"></a>
+				<h3>Doctor 1</h3>
+			</div>
+			<div class="col-sm-4">
+				<a href=""><img src="<?php echo base_url() ?>assets/img/doctor.jpg"></a>
+				<h3>Doctor 1</h3>
+			</div>
+			<div class="col-sm-4">
+				<a href=""><img src="<?php echo base_url() ?>assets/img/doctor.jpg"></a>
+				<h3>Doctor 1</h3>
+			</div>
+		</div>
+	</section>
+	<section id="o_services">
 		<h1 class="text-center">
 			Our Services
 		</h1>
@@ -43,23 +60,6 @@
 			</div>
 			<div class="col-sm-4 serve text-center">
 				<a href=""><i class="fa fa-history fa-fw fa-5x histo"></i></a><br><a href=""><h4>History</h4></a>
-			</div>
-		</div>
-	</section>
-	<section id="best">
-		<h1 class="text-center">Here Are  Our Best Doctors of The Month</h1>
-		<div class="row text-center doc">
-			<div class="col-sm-4">
-				<a href=""><img src="<?php echo base_url() ?>assets/img/doctor.jpg"></a>
-				<h3>Doctor 1</h3>
-			</div>
-			<div class="col-sm-4">
-				<a href=""><img src="<?php echo base_url() ?>assets/img/doctor.jpg"></a>
-				<h3>Doctor 1</h3>
-			</div>
-			<div class="col-sm-4">
-				<a href=""><img src="<?php echo base_url() ?>assets/img/doctor.jpg"></a>
-				<h3>Doctor 1</h3>
 			</div>
 		</div>
 	</section>
