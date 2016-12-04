4356378f472ab883c694f175a083a6c9acf4ba0e
Kenedy Lukito <kenedylukito@yahoo.com>
2016-12-04 19:42:43 +0700

    homepage laggy fix effort 2

--- assets/js/fade.js
+++ assets/js/fade.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
             }
             
         });
-        $('.fadesleft, .fadesright').each( function(i){
+        $('.fadesleft').each( function(i){
             
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -43,6 +43,22 @@ $(document).ready(function() {
                 $(this).animate({
                     'opacity':'1',
                     'left': '0px'
+                },700);
+                
+            }
+            
+        });
+        $('.fadesright').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            bottom_of_window = bottom_of_window + 50;
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({
+                    'opacity':'1',
+                    'right': '0px'
                 },700);
                 
             }
