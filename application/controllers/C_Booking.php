79987a019918db08b84759f17122dc1033d121ac
sxio <stephen.huang96@gmail.com>
2016-12-14 16:09:53 +0700

    rapikan tag html booking.php

--- application/controllers/C_Booking.php
+++ application/controllers/C_Booking.php
@@ -9,12 +9,13 @@
 			$data['title'] = 'Booking Transactions';
 			$data['header'] = $this->load->view('templates/header','',TRUE);
 			$data['nav'] = $this->load->view('templates/nav','',TRUE);
-			$data['hist'] = $this->load->Booking_model->get_booking_hist();//13-Dec-16 Meikelwis get data
 			$data['footer'] = $this->load->view('templates/footer','',TRUE);
 			$data['chat'] = $this->load->view('templates/chat','',TRUE);
 
+			$data['hist'] = $this->load->Booking_model->get_booking_hist();//13-Dec-16 Meikelwis get data
+
 			$this->load->view('booking/booking',$data);
 		}
-		
+
 	}
 ?>
