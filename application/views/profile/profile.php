418abfb0059b9aa5fdec33fd9494b54b6213f9fb
meikelwis <meikelwis>
2016-12-14 12:31:08 +0700

    Tambah dan Rapikan Validasi

--- application/views/profile/profile.php
+++ application/views/profile/profile.php
@@ -59,19 +59,19 @@
 											</ul>
 											<li>Rating</li>
 											<div class="progress">
-	  										<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:70%">
-	    									70%
-	 										</div>
-	 										<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:20%">
-	    									20%
-	 										</div>
-	 										<div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:7%">
-	    									7%
-	 										</div>
-	 										<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:3%">
-	    									3%
-	 										</div>
-										</div>
+												<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:70%">
+												70%
+												</div>
+												<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:20%">
+												20%
+												</div>
+												<div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:7%">
+												7%
+												</div>
+												<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width:3%">
+												3%
+												</div>
+											</div>
 										</ul>
 									</div>
 								</div>
