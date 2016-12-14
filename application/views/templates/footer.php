f3a7a1e64301b9ef50b9fd896c7838415b2c6171
sxio <stephen.huang96@gmail.com>
2016-12-14 16:41:43 +0700

    merapikan footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,14 +2,13 @@
 		<!-- <h1 class="text-center"></h1> -->
 		<!-- <h4 class="info text-center">This is our information</h4> -->
 	<section id="ftop">
-		
+
 		<div class="row">
 			<div class="col-sm-3 surel">
 				<ul class="list-unstyled">
-					<li><a href="<?php echo base_url('about_us') ?>"><i class="fa fa-question fa-2x roundabout" aria-hidden="true"></i></a>&nbsp;<a href="<?php echo base_url('about_us') ?>" class="inform">About Founders</a></li>
+					<li><a href="<?php echo base_url('about_us'); ?>"><i class="fa fa-question fa-2x roundabout" aria-hidden="true"></i></a>&nbsp;<a href="<?php echo base_url('about_us'); ?>" class="inform">About Founders</a></li>
 					<li><a href=""><i class="fa fa-map-marker fa-2x oval" aria-hidden="true"></i></a>&nbsp;<a href="" class="inform">Mikroskil Gedung B</a></li>
-					<!-- <li><a href=""><i class="fa fa-phone fa-2x oval_t" aria-hidden="true"></i></a>&nbsp;<a href="" class="inform">(+62)812345678910</a></li> -->
-					<li><a href=""><i class="fa fa-envelope fa-2x"></i></a> <a href="" class="inform">stephen.huang96@gmail.com</a></li>
+					<li><a href=""><i class="fa fa-envelope fa-2x"></i></a> <a href="" class="inform">avocado.mails@yahoo.com</a></li>
 					<li><a href=""><i class="fa fa-facebook-official fa-2x round" aria-hidden="true"></i></a>&nbsp;<a href="" class="inform">Avocado</a></li>
 					<li><a href=""><i class="fa fa-twitter fa-2x round" aria-hidden="true"></i></a>&nbsp;<a href="" class="inform">Avocado</a></li>
 					<li><a href=""><i class="fa fa-instagram fa-2x round" aria-hidden="true"></i></a>&nbsp;<a href="" class="inform">Avocado</a></li>
@@ -17,20 +16,11 @@
 			</div>
 			<div class="col-sm-6 corporated">
 				<h2 class="payment text-center">Payment Partner :</h2>
-					<img class="bca" src="<?php echo base_url() ?>assets/img/banks.png">
-						
+				<img class="bca" src="<?php echo base_url('assets/img/banks.png'); ?>">
 			</div>
 			<div class="col-sm-3 services">
 				<h3>Donation Report</h3>
 				<table class="table">
-			    <!-- <thead>
-			     	<tr>
-			       		<th>Firstname</th>
-			        	<th>Lastname</th>
-			        	<th>Email</th>
-			      	</tr>
-			    </thead> -->
-			    <tbody>
 					<tr>
 						<td>Today`s Donation</td>
 						<td>1,000,000</td>
@@ -43,18 +33,16 @@
 						<td>Donation Spent</td>
 						<td>254,500,000</td>
 					</tr>
-			    </tbody>
 			  </table>
 			  <div class="who">
 			  	<h4 class="d-partner text-center">Donation Partner : </h4>
-			  	<img class="img-responsive" src="<?php echo base_url() ?>assets/img/who.png">
+			  	<img class="img-responsive" src="<?php echo base_url('assets/img/who.png'); ?>">
 			  </div>
 			</div>
 		</div>
 	</section>
 	<div class="cpr">
-		<p class="pptc"><a href="">Privacy Policy</a> | <a href="">Terms & Conditions</a></p>
-		<p class="cpry">Copyright &copy; Avocado 2017.
-		 All Rights Reserved. </p>
+		<p class="pptc"><a href="">Privacy Policy</a> | <a href="">Terms &amp; Conditions</a></p>
+		<p class="cpry">Copyright &copy; Avocado 2017. All Rights Reserved. </p>
 	</div>
 	</footer>
