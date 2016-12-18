8078d1e461e0d1193a8b5d2910d2ed52bb76935d
meikelwis <meikelwis.wijaya@gmail.com>
2016-12-18 20:12:38 +0700

    Hilangkan heading tak penting di forum

--- application/views/forum/forumnav.php
+++ application/views/forum/forumnav.php
@@ -2,7 +2,6 @@
 
 <div class="page-header">
 	<ol class="breadcrumb">
-	  <li><h2>Forum</h2></li>
 	  <li><a href="<?php echo base_url('forum/forumdetail'); ?>" >Forums</a></li>
 	  <li><a href="<?php echo base_url('forum/forumdetail'); ?>" >List of topics</a></li>
 	  <li><a href="<?php echo base_url('forum/dashboard'); ?>" >Profile</a></li>
