d1df39c90a1f5b163dd6f79d1702069f52283eb6
sxio <stephen.huang96@gmail.com>
2017-01-01 23:14:30 +0700

    admin manage list order

--- application/views/admin/estore/paymentlist.php
+++ application/views/admin/estore/paymentlist.php
@@ -3,6 +3,18 @@
 <head>
 	<title>Admin | Payment List</title>
 	<?php echo $admin_header; ?>
+	<style type="text/css">
+		table th{
+			background-color: #eee;
+			text-align: center;
+		}
+		table tr td{
+			vertical-align: middle!important;
+		}
+		table tr td:last-child{
+			width: 110px;
+		}
+	</style>
 </head>
 <body>
 	<?php echo $admin_nav; ?>
@@ -17,7 +29,39 @@
 
 	<div class="container">
 		<div class="row">
-
+			<table class="table table-condensed table-bordered table-responsive">
+				<tr>
+					<th>Order ID</th>
+					<th>Delivery Address</th>
+					<th>Bank</th>
+					<th>Bank Acc Name</th>
+					<th>Bank Acc Num</th>
+					<th>Delivery</th>
+					<th>Status</th>
+					<th>Action</th>
+				</tr>
+				<?php foreach($elist as $list): ?>
+				<tr>
+					<td><?php echo $list['ORDER_ID']; ?></td>
+					<td><?php echo $list['ORDER_ADDRESS']; ?></td>
+					<td><?php echo $list['BANK_TYPE']; ?></td>
+					<td><?php echo $list['BANK_ACC_NAME']; ?></td>
+					<td><?php echo $list['BANK_ACC_NUM']; ?></td>
+					<td><?php echo $list['TRANSPORT']; ?></td>
+					<?php
+						if    ($list['STATUS'] == 'PENDING')    $label = 'warning';
+						elseif($list['STATUS'] == 'ACCEPTED')   $label = 'success';
+						elseif($list['STATUS'] == 'REJECTED')   $label = 'danger';
+						elseif($list['STATUS'] == 'ON THE WAY') $label = 'info';
+					?>
+					<td><span class="label label-<?php echo $label; ?>"><?php echo $list['STATUS']; ?></span></td>
+					<td>
+						<button class="btn btn-info"><i class="fa fa-list fa-fw"></i></button>
+						<button class="btn btn-success"><i class="fa fa-pencil fa-fw"></i></button>
+					</td>
+				</tr>
+				<?php endforeach; ?>
+			</table>
 		</div>
 	</div>
 </body>
