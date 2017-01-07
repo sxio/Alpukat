f8fc8c0e9ac826cf8bb76f1c632b3162731a8c34
meikelwis <meikelwis.wijaya@gmail.com>
2017-01-07 18:47:03 +0700

    Forum1/2jadi

--- application/views/forum/forumnav.php
+++ application/views/forum/forumnav.php
@@ -2,12 +2,12 @@
 
 <div id="forum-nav" class="container">
 	<div class="page-header">
-		<ol class="breadcrumb">
+		<ul class="list-unstyled list-inline">
 		  <li><a href="<?php echo base_url('profile/dashboard'); ?>" >Profile</a></li>
 		  <li><a href="<?php echo base_url('forum/forumhome'); ?>" >Home</a></li>
 		  <li><a href="<?php echo base_url('forum/forumcreate'); ?>" >Create Forum</a></li>
 		  <li><a href="<?php echo base_url('forum/forumlist'); ?>" >List of Topics</a></li>
 		  <li><a href="<?php echo base_url('forum/forumdetail'); ?>" >Forums</a></li>
-		</ol>
+		</ul>
 	</div>
 </div>
