9ad48ba5657a42c38bc3a7827fdf59bd05432de2
meikelwis <meikelwis.wijaya@gmail.com>
2017-01-08 15:46:31 +0700

    CreateForumCompleted

--- application/views/forum/forumcreate.php
+++ application/views/forum/forumcreate.php
@@ -7,6 +7,7 @@
 	<?php echo link_tag('assets/css/forum/forum.css'); ?>
 
 	<script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
+	<script src="//cdn.ckeditor.com/4.6.1/standard/ckeditor.js"></script>
 </head>
 <body>
 	<?php echo $nav; ?>
@@ -17,13 +18,13 @@
 			<div class="row">
 				<div class="col-sm-8">
 					<?php echo form_open('forum/createforum'); ?>
-                        <h4>Forum Topics</h4>
-                        <?php echo form_dropdown('category', $category_list, set_value('category','1'),'class="category form-control"');?>
+						<h4>Forum Topics</h4>
+						<?php echo form_dropdown('category', $category_list, set_value('category','1'),'class="category form-control"');?>
 						<br>
-                        <h4>Forum Title</h4>
-                        <textarea class="form-control" rows="2" name="title"></textarea>
-                        <h4>Forum Content</h4>
-						<textarea class="form-control" rows="8" name="content"></textarea>
+						<h4>Forum Title</h4>
+						<textarea class="form-control" rows="2" name="title"></textarea>
+						<h4>Forum Content</h4>
+						<textarea  name="forumContent" class="form-control" rows="8" name="content"></textarea>
 						<br>
 						<span class="input-group-btn">
 							<button class="btn btn-info btn-sm pull-right" id="btn-submit">
@@ -32,14 +33,22 @@
 						</span>
 					<?php echo form_close(); ?>
 				</div>
-                <div class="col-sm-4">
-                    <?php if(isset($form_error) && $form_error != null) { ?>
-                        <div class="alert alert-danger text-center"><?php echo $form_error; ?></div>
-                    <?php } ?>
-                    <?php if(isset($msg) && $msg != null) echo $msg; ?>
-                </div>
-            </div>
+				<div class="col-sm-4">
+					<?php if(isset($form_error) && $form_error != null) { ?>
+						<div class="alert alert-danger text-center"><?php echo $form_error; ?></div>
+					<?php } ?>
+					<?php if(isset($msg) && $msg != null) echo $msg; ?>
+				</div>
+			</div>
 		</div>
 	</div>
+	<script type="text/javascript">
+		CKEDITOR.replace('forumContent');
+
+		document.getElementById('image').onchange = function(event){
+			var img = document.getElementById('preview');
+			img.src = URL.createObjectURL(event.target.files[0]);
+		}
+	</script>
 </body>
 </html>
