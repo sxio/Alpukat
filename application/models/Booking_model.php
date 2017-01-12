543c52d8bae5256a423b24e264217d6c6fbdb324
Kenedy Lukito <kenedylukito@yahoo.com>
2017-01-12 18:52:42 +0700

    +Booking

--- application/models/Booking_model.php
+++ application/models/Booking_model.php
@@ -6,31 +6,36 @@
 			$this->load->helper('date');
 			date_default_timezone_set('Asia/Jakarta');
 		}
-		public function get_temp_booking_id(){
+		public function get_doctor(){
+			$this->db->select('DCT_ID');
+			$this->db->from('MSHDOCTOR');
+			$this->db->order_by('DCT_ID');
+			$result = $this->db->get();
+			$return = array();
+			if($result->num_rows() > 0){
+				foreach ($result->result_array() as $row) {
+					$return[$row['DCT_ID']] = $row['DCT_ID'];
+				}
+			};
+			return $return;
+		}
+		public function get_booking_id(){
 			$bookingid = $this->Sequences_model->concat(4, mdate('%Y-%m-%d %H:%i:%s',now()));
 			return $bookingid;
 		}
-		public function add_temp_booking($username,$bookingid){
+		public function add_booking($username,$bookingid){
 			$data = array(
-				'ID' => $bookingid,
-				'DT' => $this->input->post('bookingdt'),
-				'VALUE3' => $username,
-				'VALUE5' => mdate('%Y-%m-%d %H:%i:%s',now())
+				'BOOKING_ID' => $bookingid,
+				'BOOKING_DT' => $this->input->post('bookingdt'),
+				'DCT_ID' => $this->input->post('doctorid'),
+				'SERV_ID' => $this->input->post('servid'),
+				'BOOK_COUNT' => $this->input->post('count'),
+				'USER_ID' => $username,
+				'USER_DT' => mdate('%Y-%m-%d %H:%i:%s',now())
 			);
-			$this->db->insert('TMPHEADER',$data);
+			$this->db->insert('TRDBOOKING',$data);
 			$this->Sequences_model->update_seq(4);
 			return $this->db->error();
 		}
-		// public function add_temp_detail_booking($username){
-		// 	$data = array(
-		// 		'ID' => $booking_id,
-		// 		'DT' => $this->input->post('bookingdt'),
-		// 		'VALUE3' => $username,
-		// 		'VALUE5' => mdate('%Y-%m-%d %H:%i:%s',now())
-		// 	);
-		// 	$this->db->insert('TMPHEADER',$data);
-		// 	$this->Sequences_model->update_seq(4);
-		// 	return $this->db->error();
-		// }
 	}
 ?>
